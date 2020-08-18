8d95704b5535d9977426d2a881ac70ff12080ada
Patın Aga <69606916+batinefe@users.noreply.github.com>
2020-08-18 13:52:10 +0300

    batinin hesaplamatör
    bu kodda ürün yerleştirme bulunmaktadır

--- codes/Batinin hesaplamatör.py
+++ codes/Batinin hesaplamatör.py
@@ -1,8 +1,17 @@
 mrogramcıpatınaga=1
+def asalsayılarıbul(sayi1, sayi2):
+    for sayi in range(sayi1, sayi2 + 1):
+        if sayi > 1:
+            for i in range(2, sayi):
+                if (sayi % i == 0):
+                    break
+            else:
+                print(sayi)
 while mrogramcıpatınaga==1:
     print("""Dikdörtgen Alanı ve Çevresi Bulmak İçin 1'e
 Vize ve Final Notu Ortalaması Hesaplamak İçin 2'Ye
-Programı Kapatmak İçin 3'e Basınız""")
+2 Sayı Arasındaki Asal Sayıları Bulmak İçin 3'e
+Programı Kapatmak İçin 4'e""")
     secim=int(input("HAĞNGİSİ:"))
     if secim==2:
         vize=int(input("Vize Notunuzu Giriniz:"))
@@ -19,11 +28,9 @@ Programı Kapatmak İçin 3'e Basınız""")
             print("Alan:{}".format(alan))
             print("Çevre:{}".format(çevre))
             break
-    elif secim==3:
+    elif secim==4:
         mrogramcıpatınaga=0
         break
-        
-        
-        
-        
-                 
+    elif secim==3:
+        mrogramcıpatınaga=1
+        asalsayılarıbul(int(input('sayı 1:')),int(input('sayı 2:')))
