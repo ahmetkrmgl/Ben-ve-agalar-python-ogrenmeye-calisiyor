24e47c7175f2dd6fddc928a6442c99206ea1b1f3
Ahmet Kırmagül <67853584+ahmetkrmgl@users.noreply.github.com>
2020-08-20 00:27:04 +0300

    Add files via upload

--- codes/Müzik Listesi Oluşturma.py
+++ codes/Müzik Listesi Oluşturma.py
@@ -1,9 +1,12 @@
-liste=[""]
+import os
+secim=input("Dosyayı sıfırlamak için evet yazın(istemiyorsanız entera basın):")
+if secim=="evet":
+    os.remove("müziklistesi.txt")
+liste=open("müziklistesi.txt","w")
 music=""
-liste[0]=input("Bir müzik giriniz:")
+liste.write("{} \n".format(input("Bir müzik giriniz:")))
 while music !="yazmayı durdur":
     music=input("Bir müzik giriniz:")
-    liste.append(music)
-liste.pop()
-for x in range(len(liste)):
-    print(liste[x].title())
+    if music!="yazmayı durdur":
+        liste.write("{} \n".format(music))
+liste.close()
